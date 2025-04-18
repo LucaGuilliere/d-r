722890e32933c998f7fcd222d48384b38bc2fab6
Luca Guilliere <lucaguilliere@Mac.lan>
2025-04-18 22:43:50 +0200

    [ADD] begin real values implementation

--- app/src/main/java/com/example/mymeteomockup/MainActivity.kt
+++ app/src/main/java/com/example/mymeteomockup/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.mymeteomockup
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    /*private var humidityTF= findViewById<TextView>(R.id.humidityTF)
+    private var visibilityTF= findViewById<TextView>(R.id.visibilityTF)
+    private var windTF= findViewById<TextView>(R.id.windTF)*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        /*val windSpeed = location.weather.wind.speed
+        val windKmH = (windSpeed * 3.6).roundToInt()
+        windTF.setText("$windKmH km/h")
+
+        val humidity = location.weather.main.humidity
+        humidityTF.setText("$humidity%")
+
+        val visibilityMeters = location.weather.visibility
+        val visibilityKm = visibilityMeters / 1000.0
+        visibilityTF.setText("$visibilityKm km")*/
+
     }
 }
